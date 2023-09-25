5d78ba9fa16aa7f33d6357217e688cacf76da452
probr1anz <145923602+probr1anz@users.noreply.github.com>
2023-09-25 20:33:59 +0800

    Update median.py

--- median.py
+++ median.py
@@ -10,3 +10,16 @@ while True:
     else:
         break
 print(numbers)
+
+
+
+total = len(numbers)
+numbers.sort()
+
+if total % 2 == 0:
+	median1 = numbers[total//2]
+	median2 = numbers[total//2 - 1]
+	median = (median1 + median2)/2
+else:
+	median = numbers[total//2]
+print("Median is: " + str(median))
